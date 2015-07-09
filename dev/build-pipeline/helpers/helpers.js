abe62151456c38ba4976bd253faf94279d70e358
Matthew Trost <matthew@famo.us>
2015-07-09 13:50:52 -0700

    wip: Build refactor - remove irrelevant helper functions from build helper functions module

--- dev/build-pipeline/helpers/helpers.js
+++ dev/build-pipeline/helpers/helpers.js
@@ -1,57 +1,11 @@
 'use strict';
 
-var Async = require('async');
-var Fs = require('fs');
 var Lodash = require('lodash');
-var Path = require('path');
-var ReaddirRecursive = require('recursive-readdir');
 
 var Config = require('./../config/config');
 
 var HTTP_REGEXP = /^https?:\/\//i;
 var STRING_TYPE = 'string';
-
-function readFilesRecursive(baseDir, finish) {
-    ReaddirRecursive(baseDir, function(dirErr, filePaths) {
-        if (dirErr) {
-            return finish(dirErr);
-        }
-
-        Async.map(filePaths, Fs.readFile, function(contentsErr, fileContents) {
-            if (contentsErr) {
-                return finish(contentsErr);
-            }
-
-            var resultsArray = [];
-
-            for (var i = 0; i < fileContents.length; i++) {
-                var filePath = filePaths[i].replace(baseDir, '');
-                var fileContent = fileContents[i];
-
-                // Remove the preceding slash since the users of this
-                // function downstream expect the path to be relative
-                if (filePath[0] === Path.sep) {
-                    filePath = filePath.slice(1, filePath.length);
-                }
-
-                var fileData;
-                if (doesFileLookLikeBinary(filePath)) {
-                    fileData = new Buffer(fileContent).toString('base64');
-                }
-                else {
-                    fileData = fileContent;
-                }
-
-                resultsArray.push({
-                    path: filePath,
-                    content: fileData
-                });
-            }
-
-            return finish(null, resultsArray);
-        });
-    });
-}
 
 function dependencyStringToModuleName(str) {
     var parts = moduleNameToModuleNameSegments(str);
@@ -80,21 +34,13 @@ function eachDependencyStringInString(str, iterator) {
     }
 }
 
-function doesFileLookLikeStaticAsset(fileObject) {
-    return !!Config.get('assetTypes')[Path.extname(fileObject.path)];
-}
-
-function doesFileLookLikeBinary(fileObject) {
-    return !!Config.get('binaryTypes')[Path.extname(fileObject.path)];
-}
-
 function moduleNameToEntrypointBasename(moduleName) {
     var moduleNameParts = moduleNameToModuleNameSegments(moduleName);
     return moduleNameParts[moduleNameParts.length - 1];
 }
 
 function moduleNameToModuleNameSegments(moduleName) {
-  return moduleName.split(Config.get('componentDelimiter'));
+    return moduleName.split(Config.get('componentDelimiter'));
 }
 
 function importsObjectToFlatImportsObject(importsObj) {
@@ -115,57 +61,17 @@ function moduleNamespaceAndBasenameToModuleName(moduleNamespace, moduleEntrypoin
     return moduleNamespace + Config.get('componentDelimiter') + moduleEntrypointBasename;
 }
 
-function buildIncludesArray(data, skipURLExpansion) {
-    var moduleConfigObjects = data.moduleConfigObjects;
-    var inlinedFiles = data.inlinedFiles || [];
-    var includesArray = [];
-
-    for (var i = 0; i < moduleConfigObjects.length; i++) {
-        var moduleConfig = moduleConfigObjects[i];
-
-        if (moduleConfig.includes) {
-            for (var j = 0; j < moduleConfig.includes.length; j++) {
-                var includeStr = moduleConfig.includes[j];
-                // Someone might want to get the 'raw' includes array without
-                // any expansion e.g. ['foo.js', 'bar.css']
-                if (skipURLExpansion) {
-                    includesArray.push(includeStr);
-                }
-                else {
-                    // No need to 'include' any files that have been already
-                    // inlined inside of the entrypoint
-                    if (inlinedFiles.indexOf(includeStr) === -1) {
-                        // If a full URL was given, don't prefix it with the version URL
-                        if (HTTP_REGEXP.test(includeStr)) {
-                            includesArray.push(includeStr);
-                        }
-                        else {
-                            console.warn('Ambiguous include string ', includeStr);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    return Lodash.uniq(includesArray);
-}
-
 function moduleNameToNamespace(moduleName) {
     return Lodash.first(moduleName.split(Config.get('componentDelimiter')));
 }
 
 module.exports = {
-    buildIncludesArray: buildIncludesArray,
     dependencyStringToModuleName: dependencyStringToModuleName,
-    doesFileLookLikeStaticAsset: doesFileLookLikeStaticAsset,
-    doesFileLookLikeBinary: doesFileLookLikeBinary,
     doesStringLookLikeDependency: doesStringLookLikeDependency,
     eachDependencyStringInString: eachDependencyStringInString,
     importsObjectToFlatImportsObject: importsObjectToFlatImportsObject,
     moduleNameToNamespace: moduleNameToNamespace,
     moduleNamespaceAndBasenameToModuleName: moduleNamespaceAndBasenameToModuleName,
     moduleNameToEntrypointBasename: moduleNameToEntrypointBasename,
-    moduleNameToModuleNameSegments: moduleNameToModuleNameSegments,
-    readFilesRecursive: readFilesRecursive
+    moduleNameToModuleNameSegments: moduleNameToModuleNameSegments
 };
