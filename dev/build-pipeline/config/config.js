6fb884efa72cded38038043cd0a9cde5e9af6d63
Matthew Trost <matthew@famo.us>
2015-07-09 13:50:25 -0700

    wip: Build refactor - remove irrelevant config info from the build config file

--- dev/build-pipeline/config/config.js
+++ dev/build-pipeline/config/config.js
@@ -3,16 +3,7 @@
 var Lodash = require('lodash');
 
 var options = {
-    localSourceFolder: null,
     defaultDependencyVersion: 'HEAD',
-    frameworkBundleURL: 'famous-framework.bundle.js',
-    fileOptions: { encoding: 'utf8' },
-    // Content and assets
-    assetTypes: { '.eot': true, '.gif': true, '.ico': true, '.jpeg': true, '.jpg': true, '.otf': true, '.png': true, '.svg': true, '.ttf': true, '.txt': true, '.woff': true, '.woff2': true },
-    binaryTypes: { '.eot': true, '.gif': true, '.ico': true, '.jpeg': true, '.jpg': true, '.otf': true, '.png': true, '.ttf': true, '.woff': true, '.woff2': true },
-    contentTypeFallback: 'application/octet-stream',
-    contentTypeMap: { '.css': 'text/css', '.eot': 'application/vnd.ms-fontobject', '.gif': 'image/gif', '.html': 'text/html', '.htm': 'text/html', '.ico': 'image/x-icon', '.jpeg': 'image/jpeg', '.jpg': 'image/jpeg', '.js': 'application/javascript', '.json': 'application/json', '.markdown': 'text/x-markdown', '.md': 'text/x-markdown', '.otf': 'font/opentype', '.png': 'image/png', '.svg': 'image/svg+xml', '.ttf': 'application/octet-stream', '.txt': 'text/plain', '.woff': 'application/x-font-woff', '.woff2': 'application/font-woff2', '.xhtml': 'text/html', '.xml': 'application/xml' },
-    // Parsing / processing
     attachmentIdentifiers: { 'attach': true },
     baseURLToken: '{{BASE_URL}}',
     behaviorsFacetKeyName: 'behaviors',
@@ -21,36 +12,24 @@ var options = {
     componentDelimiterRegexp: /:/g,
     configMethodIdentifier: 'config', // e.g. FamousFramework.scene(...).config({...})
     defaultExtends: ['famous:core:node'],
-    defaultImports: {
+    defaultShorthands: {
         'famous:core': [ 'node' ],
         'famous:events': [
-            // FamousEngine supported events
-            'abort', 'beforeinput', 'blur', 'click', 'compositionend', 'compositionstart',
+            'abort', 'beforeinput', 'blur', 'click', 'compositionend', 'compositionstart', // FamousEngine supported events
             'compositionupdate', 'dblclick', 'focus', 'focusin', 'focusout', 'input',
             'keydown', 'keyup', 'mousedown', 'mouseenter', 'mouseleave', 'mousemove', 'mouseout',
             'mouseover', 'mouseup', 'scroll', 'select', 'wheel', 'touchcancel',
             'touchend', 'touchmove ', 'touchstart',
-            // Gestures
-            'drag', 'tap', 'pinch', 'rotate',
-            // Size Events
-            'size-change', 'parent-size-change'
+            'drag', 'tap', 'pinch', 'rotate', // Gestures
+            'size-change', 'parent-size-change' // Size Events
         ]
     },
     dependencyBlacklist: { 'localhost': true },
     dependencyRegexp: /([\w-_.]+:)+(([\w-_.]+(?=[\s|>|\/]))|([\w-_.]+(?=:)))/ig,
     entrypointExtnames: { '.js': true },
     eventsFacetKeyName: 'events',
-    includesKeyName: 'includes',
-    importsKeyName: 'imports', // e.g. FamousFramework.scene(...).config({imports:{...}})
-    indexOfModuleNameArgument: 0, // e.g. FamousFramework.scene('THIS STRING', {...})
-    indexOfModuleDefinitionArgument: 1, // e.g. FamousFramework.scene('foo'. {THIS OBJECT})
-    indexOfModuleConfigArgument: 0, // e.g. FamousFramework.scene(...).config({THIS OBJECT})
-    libraryInvocationIdentifiers: {
-        'module': true, // FamousFramework.module(...) // All these are equivalent on the client
-        'component': true, // FamousFramework.component(...)
-        'scene': true // FamousFramework.scene(...)
-    },
-    libraryMainNamespace: 'FamousFramework',
+    shorthandsKeyName: 'shorthand',
+    libraryInvocationIdentifiers: { 'component': true },
     passThroughKey: '$pass-through',
     reservedEventValues: {},
     treeFacetKeyName: 'tree'
